1d42c24c6c2491f5c9da0c50bcb3e09b7fcfdf5d
Dan Abramov <dan.abramov@gmail.com>
2025-11-30 16:57:33 +0900

    nice

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,28 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+const pending = new Map<string, Promise<any>>();
+
+async function dedup(key: string): Promise<string> {
+  let existingPromise;
+  while ((existingPromise = pending.get(key))) {
+    console.log("await existing");
+    return await existingPromise;
+  }
+
+  console.log("starting");
+  const promise = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      console.log("done");
+      resolve("ok");
+    }, 100);
+  }).finally(() => {
+    pending.delete(key);
+  });
+
+  pending.set(key, promise);
+  return promise;
+}
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  await dedup("test");
   return (
     <html>
       <body>{children}</body>
